636b97db5b07e182fa7fb7cfc2577fdb6a723942
Alejandro Morales, Angel <NGELALE+mapfre@mapfrepr.com>
2025-04-07 13:31:18 -0400

    bkp commit before presentation

---
+++ MPR.RestApiTemplate.Utilities.CodeGeneration/T4Templates/Controllers.cs
@@ -0,0 +1,3 @@
+// Esta plantilla ha generado controladores individuales para cada entidad.
+// Los archivos generados se encuentran en la carpeta GeneratedControllers.
+// Nota: Los archivos existentes no han sido sobrescritos.
--- MPR.RestApiTemplate.Utilities.CodeGeneration/T4Templates/Dtos.cs
+++ MPR.RestApiTemplate.Utilities.CodeGeneration/T4Templates/Dtos.cs
@@ -1,6 +1,1 @@
-
-
-
-// Esta plantilla ha generado archivos DTO individuales para cada entidad.
-// Los archivos generados se encuentran en la carpeta GeneratedDTOs.
-// Nota: Los archivos existentes no han sido sobrescritos.
+ErrorGeneratingOutput
