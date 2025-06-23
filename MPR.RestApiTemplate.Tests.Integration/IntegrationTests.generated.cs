35d50d39821e75a719e60a0521176e9641ce2cb2
Alejandro Morales, Angel <NGELALE+mapfre@mapfrepr.com>
2025-06-22 22:42:33 -0400

    removing .vs file

---
+++ MPR.RestApiTemplate.Tests.Integration/IntegrationTests.generated.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using MPR.RestApiTemplate.Application.DTOs;
+
+namespace MPR.RestApiTemplate.IntegrationTests
+{
+}
